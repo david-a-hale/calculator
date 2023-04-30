5d1d78d37e5db4f99228e9695339fb3728ad878b
David A Hale <115053604+david-a-hale@users.noreply.github.com>
2023-04-30 11:37:56 -0400

    initial math functions

---
+++ main.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(4, 6));
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(10, 5));
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(5, 5));
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(12, 6));
